80d7402a743dedbf09864e33b7a63e611cec5f8b
Adam Budai <budai8adam@gmail.com>
2024-07-16 19:01:51 +0200

    kernel header parser was implemented

--- tinyGPUlang/parser.cpp
+++ tinyGPUlang/parser.cpp
@@ -60,6 +60,34 @@ TGLparser::TGLparser(const std::string& path_to_tgl)
             std::cout << ntoken << "\n";
         }
     }
+    
+    int next_line;
+    int next_pos;
+    parse_next_kernel(0, 0, next_line, next_pos);
+
+    auto kernel = defined_global_kernels["calc_mse"];
+
+    std::cout << kernel->name << std::endl;
+    std::cout << (int)kernel->scope << std::endl;
+
+    for (auto v : kernel->arguments)
+    {
+        std::cout << v->name << std::endl;
+        std::cout << (int)v->dtype << std::endl;
+        std::cout << (int)v->vtype << std::endl;
+        
+        if (v->vtype == VariableType::TENSOR)
+        {
+            auto shape = std::static_pointer_cast<TensorNode>(v)->shape;
+
+            for (int s : shape)
+            {
+                std::cout << s << " ";
+            }
+            std::cout << std::endl;
+        }
+        
+    }
 }
 
 std::vector<KernelNodePtr> TGLparser::get_all_global_kernel() const
@@ -149,4 +177,202 @@ int TGLparser::parse_next_token(std::string& next_token, const std::string& line
         next_token = line.substr(first_pos, last_pos - first_pos);
     }
     return current_pos;
+}
+
+void TGLparser::parse_next_kernel(const int start_line, const int start_pos, int& next_line, int& next_pos)
+{
+    int current_line = start_line;
+    int current_pos = start_pos;
+    std::string next_token;
+    
+    // find the line with the next function header
+    bool found_func = false;
+    while (!found_func)
+    {
+        if (current_line >= all_lines.size())
+        {
+            break;
+        }
+
+        auto& cline = all_lines[current_line];
+        
+        bool cont = true;
+        while (cont)
+        {
+            current_pos = parse_next_token(next_token, cline, current_pos);
+            found_func = next_token == "func";
+            cont = !(next_token == "" || found_func);
+        }
+
+        if (!found_func)
+            current_line += 1;
+    }
+
+    // parse kernel header and save it
+    KernelNodePtr kernel = parse_kernel_header(current_line, current_pos, current_pos);
+    if (kernel == nullptr)  // no more function can be found
+    {
+        next_line = static_cast<int>(all_lines.size());
+        next_pos = 0;
+        return;
+    }
+
+    if (kernel->scope == KernelScope::GLOBAL)
+    {
+        if (defined_global_kernels.contains(kernel->name))
+        {
+            std::cout << "Global kernel already defined " << kernel->name << "\n";
+        }
+
+        defined_global_kernels.insert({kernel->name, kernel});
+    }
+    else  // device
+    {
+        if (defined_global_kernels.contains(kernel->name))
+        {
+            std::cout << "Device kernel already defined " << kernel->name << "\n";
+        }
+
+        defined_device_kernels.insert({kernel->name, kernel});
+    }
+
+    // return
+    next_line = current_line;
+    next_pos = current_pos;
+}
+
+KernelNodePtr TGLparser::parse_kernel_header(const int start_line, const int start_pos, int& next_pos)
+{
+    int current_line = start_line;
+    int current_pos = start_pos;
+    std::string next_token;
+
+    // parse the function type
+    auto& cline = all_lines[current_line];
+    current_pos = parse_next_token(next_token, cline, current_pos);
+    
+    KernelScope kernel_scope;
+    if (next_token == "global")
+    {
+        kernel_scope = KernelScope::GLOBAL;
+    }
+    else if (next_token == "device")
+    {
+        kernel_scope = KernelScope::DEVICE;
+    }
+    else
+    {
+        std::cout << "Wrong scope for function " << next_token << "\n";
+        return nullptr;
+    }
+
+    // parse the return type
+    current_pos = parse_next_token(next_token, cline, current_pos);
+
+    VariableNodePtr return_var_type = nullptr; 
+    if (next_token != "void")
+    {
+        if (next_token != "f32" || next_token != "f16")
+        {
+            std::cout << "Wrong variable type " << next_token << "\n";
+        }
+
+        return_var_type = parse_variable_type(current_line, current_pos - 3, current_pos);  // f32 or f16;
+    }
+
+    // parse the function name
+    std::string kernel_name;
+    current_pos = parse_next_token(kernel_name, cline, current_pos);
+
+    // parse argument var types
+    std::vector<VariableNodePtr> args;
+    
+    std::string start_parantheses;
+    current_pos = parse_next_token(start_parantheses, cline, current_pos);
+
+    if (start_parantheses != "(")
+    {
+        std::cout << "Expected a ( character instead of " << next_token << "\n";
+    }
+
+    while (next_token != ")")
+    {
+        auto var_type = parse_variable_type(current_line, current_pos, current_pos);
+        current_pos = parse_next_token(next_token, cline, current_pos);
+        args.push_back(var_type);
+    }
+
+    // return values
+    next_pos = current_pos;
+    return std::make_shared<KernelNode>(kernel_name, kernel_scope, args, return_var_type);
+}
+
+
+VariableNodePtr TGLparser::parse_variable_type(const int start_line, const int start_pos, int& next_pos)
+{
+    VariableNodePtr var = nullptr;
+    int current_pos = start_pos;
+    
+    std::string next_token;
+    auto& cline = all_lines[start_line];  // variable type def has to be in one line
+
+    // read the data type
+    DataType dtype;
+    current_pos = parse_next_token(next_token, cline, current_pos);
+    if (next_token == "f32")
+    {
+        dtype = DataType::FLOAT32;
+    }
+    else if (next_token == "f16")
+    {
+        dtype = DataType::FLOAT16;
+    }
+    else
+    {
+        std::cout << "Expected a f32 or f16, but got instead " << next_token << "\n";
+    }
+
+    // decide variable type
+    VariableType vtype;
+    current_pos = parse_next_token(next_token, cline, current_pos);
+    if (next_token == "[")  // this has to be a tensor
+    {
+        vtype = VariableType::TENSOR;
+    }
+    else  // has to be a scalar
+    {
+        vtype = VariableType::SCALAR;
+    }
+
+    // if tensor read the shape and the name
+    if (vtype == VariableType::TENSOR)
+    {
+        // read the shape
+        std::vector<int> shape;
+        while (next_token != "]")
+        {
+            current_pos = parse_next_token(next_token, cline, current_pos);
+            if (next_token != "]" && next_token != ",")
+            {
+                int s = atoi(next_token.c_str());
+                shape.push_back(s);
+            }
+        }
+
+        // read the var name
+        current_pos = parse_next_token(next_token, cline, current_pos);
+        std::string var_name = next_token;
+
+        var = std::make_shared<TensorNode>(vtype, dtype, var_name, shape);
+    }
+
+    // if scalar (return the type)
+    if (vtype == VariableType::SCALAR)
+    {
+        auto var_name = next_token;  // for scalar, the last read is the var. name
+        var = std::make_shared<ScalarNode>(vtype, dtype, var_name);
+    }
+    
+    next_pos = current_pos;
+    return var;
 }
--- tinyGPUlang/parser.hpp
+++ tinyGPUlang/parser.hpp
@@ -2,6 +2,7 @@
 
 #include "ast.hpp"
 #include <unordered_set>
+#include <unordered_map>
 
 class TGLparser
 {
@@ -19,7 +20,8 @@ protected:
     static std::unordered_set<char> comma_chars;
     static std::unordered_set<char> arithmetic_chars;
     static std::unordered_set<std::string> builtin_kernel_names;
-    std::unordered_set<std::string> defined_kernel_names;
+    std::unordered_map<std::string, KernelNodePtr> defined_global_kernels;
+    std::unordered_map<std::string, KernelNodePtr> defined_device_kernels;
 
     /**
      * Reads all the text from the source file.
@@ -36,5 +38,18 @@ protected:
      * @return the position where the search ended (next search should start here)
      */
     int parse_next_token(std::string& next_token, const std::string& line, const int start_pos);
+
+    /**
+     * Parse the next function from the source file.
+     * @param start_line the first line to look for the next kernel def
+     * @param start_pos first position within the line
+     * @return the line where the search ended (next search should start here)
+     */
+    void parse_next_kernel(const int start_line, const int start_pos, int& next_line, int& next_pos);
+
+    KernelNodePtr parse_kernel_header(const int start_line, const int start_pos, int& next_pos);
+    void parse_kernel_body(KernelNodePtr kernel, const int start_line, const int start_pos, int& next_line, int& next_pos);
+
+    VariableNodePtr parse_variable_type(const int start_line, const int start_pos, int& next_pos);
 
 };
