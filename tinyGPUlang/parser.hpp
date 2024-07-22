0613bdaf91f8f3f4241ebbcbbfea94d5c30c8716
Adam Budai <budai8adam@gmail.com>
2024-07-22 16:39:40 +0200

    more comment for parser

--- tinyGPUlang/parser.hpp
+++ tinyGPUlang/parser.hpp
@@ -46,18 +46,38 @@ protected:
      * Parse the next function from the source file.
      * @param start_line the first line to look for the next kernel def
      * @param start_pos first position within the line
-     * @return the line where the search ended (next search should start here)
+     * @param next_line the line where the search ended (next search should start here)
+     * @param next_pos the position where the search ended (next search should start here)
      */
     void parse_next_kernel(const int start_line, const int start_pos, int& next_line, int& next_pos);
 
+    /**
+     * Reads the kernel header (gives the definition of the kernel).
+     * The kernel header should be in a single line.
+     */ 
     KernelNodePtr parse_kernel_header(const int start_line, const int start_pos, int& next_pos);
+    
+    /**
+     * Reads the body of the kernel, each line (expression) will
+     * be an ASTNode. The kernel body is among curly brackets.
+     */
     void parse_kernel_body(KernelNodePtr kernel, const int start_line, const int start_pos, int& next_line, int& next_pos);
-
+    
+    /**
+     * Reads the type of a variable. Type definition can happen in
+     * a kernel header. The name will remain empty.
+     */
     VariableNodePtr parse_variable_type(const int start_line, const int start_pos, int& next_pos);
 
+    /**
+     * Reads a constant value in the code (a value given inside the code).
+     * Only floats are supported.
+     */
     ConstantNodePtr parse_constant_scalar(const std::string& value_as_string);
     
     /**
+     * Reads the var t = arithm expr.; like code pieces.
+     * The alias will be for the ASTNode built from the arithmetic expression.
      * @param start_pos shows the position at the beginning of the alias name.
      */
     AliasNodePtr parse_alias_node( 
@@ -66,6 +86,7 @@ protected:
         int& next_pos);
 
     /**
+     * Reads the return arithm expr.; like code pieces.
      * @param start_pos shows the position at the beginning of the variable name.
      */
     ReturnNodePtr parse_return_node( 
@@ -74,6 +95,7 @@ protected:
         int& next_pos);
 
     /**
+     * Reads the d = arithm expr.; like code pieces.
      * @param start_pos shows the position at the beginning of the arithmetic ops.
      */
     AssignmentNodePtr parse_assignment_node(
@@ -81,8 +103,11 @@ protected:
         const int start_line, 
         const int start_pos, 
         int& next_pos);
-    
+
     /**
+     * Reads a function call. The call is to a device function
+     * defined in the same tgl file, somewhere earlier.
+     * Builtin function calls are handled as arithmetic expressions.
      * @param start_pos shows the position at the beginning of the args.
      */
     ASTNodePtr parse_kernel_call_node(
@@ -92,7 +117,9 @@ protected:
         int& next_pos);
 
     /**
-     * @param start_pos shows the position at the beginning of the args.
+     * Can be a function call (newly defined are delegated), and regular
+     * mathematical expressions. (E.g.: a + (b * c) - abs(d) + ((a / b) + d + 2.0); )
+     * @param start_pos shows the position at the beginning of the expression.
      */
     ASTNodePtr parse_arithmetic_node( 
         const int start_line, 
